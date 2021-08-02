f45897196f81bb1e6e7eb0f76a589d4a0081701b
akhil2502 <akhilachilies>
2021-08-02 19:17:55 +0530

    [akhil] Add:uc2-new-contact

--- Program.cs
+++ Program.cs
@@ -1,42 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
-namespace Address_Book.Address_book
+namespace AddressBook
 {
-    class addressbook
+    class Program
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Address { get; set; }
-        public string City { get; set; }
-        public string State { get; set; }
-        public double Zip { get; set; }
-        public double PhoneNumber { get; set; }
-        public string Email { get; set; }
-        public void displayContact()
-        {
-            Console.WriteLine("FirstName: " + this.FirstName + " LastName: " + this.LastName + " Address: " + this.Address +
-                                " City: " + this.City + " State: " + this.State + " Zip: " + this.Zip +
-                                  " PhoneNumber: " + this.PhoneNumber + " Email: " + this.Email);
-        }
         static void Main(string[] args)
         {
-            Console.Write("Enter First Name, Last Name, Address, City, State, Zip, Phone Number, Email \n");
-            addressbook program = new addressbook()
-            {
-                FirstName = Console.ReadLine(),
-                LastName = Console.ReadLine(),
-                Address = Console.ReadLine(),
-                City = Console.ReadLine(),
-                State = Console.ReadLine(),
-                Zip = Convert.ToDouble(Console.ReadLine()),
-                PhoneNumber = Convert.ToDouble(Console.ReadLine()),
-                Email = Console.ReadLine(),
-            };
-            program.displayContact();
+            ContactPerson contactPerson = new ContactPerson();
+            contactPerson.AddContact();
         }
-    }
-}
     }
 }
---
+++ new-contact.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Address_Book
+
+{
+    class ContactPerson
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public double Zip { get; set; }
+        public double PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+        public void DisplayContact()
+        {
+            Console.WriteLine("FirstName: " + this.FirstName + " LastName: " + this.LastName + " Address: " + this.Address +
+                                " City: " + this.City + " State: " + this.State + " Zip: " + this.Zip +
+                                  " PhoneNumber: " + this.PhoneNumber + " Email: " + this.Email);
+
+        }
+
+        public void AddContact()
+        {
+            Console.Write("Enter First Name, Last Name, Address, City, State, Zip, Phone Number, Email \n");
+            ContactPerson contactPerson = new ContactPerson()
+            {
+                FirstName = Console.ReadLine(),
+                LastName = Console.ReadLine(),
+                Address = Console.ReadLine(),
+                City = Console.ReadLine(),
+                State = Console.ReadLine(),
+                Zip = Convert.ToDouble(Console.ReadLine()),
+                PhoneNumber = Convert.ToDouble(Console.ReadLine()),
+                Email = Console.ReadLine(),
+            };
+            List<ContactPerson> list = new List<ContactPerson>();
+            list.Add(contactPerson);
+            contactPerson.DisplayContact();
+        }
+    }
+}
